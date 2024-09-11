1883337953ace3a062daf6774f2d24f49237bd0d
PabloRafael-coder <pabloofael@gmail.com>
2024-09-11 13:53:20 -0300

    feat: Adicione a addEventListener no botão para remover a li

--- scripts.js
+++ scripts.js
@@ -1,41 +1,36 @@
-// Pegando os elementos do html
 const form = document.getElementById("form");
 const input = document.getElementById("title")
-const p = document.querySelector(".section__text")
 const ul = document.getElementById("section__ul")
-const button = document.querySelector(".section__delete-button")
+const button = document.getElementById("button-delete")
 
-
-// Manipulando o botão da aplicação
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
     createNewElement();
+    input.value = '';
 })
 
 function createNewElement() {
+    const createLi = document.createElement("li")
+    createLi.classList.add("section__item")
 
-    const newLi = document.createElement("li")
-    newLi.classList = "section__item"
+    const createInput = document.createElement("input")
+    createInput.setAttribute("type", "checkbox");
+    createInput.classList.add("section__checked-input")
 
-    const newInput = document.createElement("input")
-    newInput.setAttribute("type", "checkbox");
-    newInput.classList = "section__checked-input";
-
-    const newP = document.createElement("p");
-    newP.classList = "section__text"
-
-    if (newP.textContent === '') {
-        newP.textContent = input.value
-    } else {
-        newP.textContent
-    }
+    const createElementP = document.createElement("p");
+    createElementP.classList.add("section__text")
 
     const newButton = document.createElement("button");
-    newButton.classList = "section__delete-button"
+    newButton.classList.add("section__delete-button")
 
-    newLi.append(newInput)
-    newLi.append(newP)
-    newLi.append(newButton)
-    ul.appendChild(newLi)
+    createElementP.textContent = input.value
+
+    createLi.append(createInput, createElementP, newButton)
+    ul.appendChild(createLi)
+
+
+    newButton.addEventListener("click", () => {
+        ul.removeChild(createLi)
+    })
 }
